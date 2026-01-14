cb4c1b0d6fc22c5ca4fb2dd02993cf467f9f84f2
Stephen Goncalves <stiefo.o@gmail.com>
2026-01-14 21:23:20 +0000

    update env var

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,5 +3,5 @@ import type { Config } from '../../payload-types'
 
 // Pass your config from generated types as generic
 export const cms = new PayloadSDK<Config>({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
 })
