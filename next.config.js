d9c7dcd0cb8006e277330a98e4fd6247c26f2648
Stephen Goncalves <stiefo.o@gmail.com>
2026-01-14 17:54:21 +0000

    update next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { withPayload } from '@payloadcms/next/withPayload'
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        reactCompiler: false,
+    }
+}
+
+module.exports = withPayload(nextConfig)
