fcbe2091f8d7a707f353c2853b04f3a8f87fce80
Stephen Goncalves <stiefo.o@gmail.com>
2026-01-14 21:51:52 +0000

    update migration

--- src/migrations/20260114_210857.ts
+++ src/migrations/20260114_210857.ts
@@ -1,4 +1,4 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
